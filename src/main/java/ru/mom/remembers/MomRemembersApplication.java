d9cd0d34b85198306fb594b7b115352b662c8d33
Solntsev Maksim <solntsev1099@gmail.com>
2023-04-28 16:51:03 +0300

    Initial commit

---
+++ src/main/java/ru/mom/remembers/MomRemembersApplication.java
@@ -0,0 +1,13 @@
+package ru.mom.remembers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomRemembersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomRemembersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mom/remembers/MomRemembersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mom.remembers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomRemembersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
